058024e8635033474a5ea6eb030d1987ab34a128
Michael Scott Hayes <98482126+Mikey1336@users.noreply.github.com>
2025-02-12 18:28:27 -0500

    Main to function that returns all data

--- main.py
+++ main.py
@@ -31,56 +31,91 @@ Comparision) Compare Marketcap, Holder to Marketcap Ratio, Volume, and Impressio
 
 Correlation) Return correlation scores to user on website
 """
-# Declare global variables
+def main(mintAdd):
+    # Declare global variables
 
-solRpcUrl = "https://api.mainnet-beta.solana.com"
-helius_api_key = "96f8d766-bb4f-4b39-b4b1-8ede9278be60"
-bonkLaunchDate = datetime.date(2023,1,5)
-wifLaunchDate = datetime.date(2023,11,21)
-fartcoinLaunchDate = datetime.date(2024,10,19)
-fwogLaunchDate = datetime.date(2024,7,30)
-gigaLaunchDate = datetime.date(2024,1,5)
-goatLaunchDate = datetime.date(2024,10,11)
-chillguyLaunchDate = datetime.date(2024,11,21)
-# Accept user input for the contract address
-newMintAdd = input("Please enter a contract address to compare: ")
+    solRpcUrl = "https://api.mainnet-beta.solana.com"
+    helius_api_key = "96f8d766-bb4f-4b39-b4b1-8ede9278be60"
+    bonkLaunchDate = datetime.date(2023,1,5)
+    wifLaunchDate = datetime.date(2023,11,21)
+    fartcoinLaunchDate = datetime.date(2024,10,19)
+    fwogLaunchDate = datetime.date(2024,7,30)
+    gigaLaunchDate = datetime.date(2024,1,5)
+    goatLaunchDate = datetime.date(2024,10,11)
+    chillguyLaunchDate = datetime.date(2024,11,21)
+    # Accept user input for the contract address
+    newMintAdd = mintAdd
 
-# Get user token age
-userTokenAge = get_token_age_from_dexscreener(newMintAdd)
+    # Get user token age
+    userTokenAge = get_token_age_from_dexscreener(newMintAdd)
 
-# Get the ticker
-userTicker = get_token_ticker_from_mint_dexscreener(newMintAdd)
-# Get user token mcap
-userTokenMcap = get_token_market_cap(newMintAdd)
-# Get user token vol
-userTokenVol = get_token_24h_volume(newMintAdd)
-# Get user token Holder to mcap
-#userTokenHolders = getHoldertoMcap.get_holder_count(solRpcUrl, newMintAdd)
-#userTokenHoldertoMcap = userTokenMcap / userTokenHolders
+    # Get the ticker
+    userTicker = get_token_ticker_from_mint_dexscreener(newMintAdd)
+    # Get user token mcap
+    userTokenMcap = get_token_market_cap(newMintAdd)
+    # Get user token vol
+    userTokenVol = get_token_24h_volume(newMintAdd)
+    # Get user token Holder to mcap
+    #userTokenHolders = getHoldertoMcap.get_holder_count(solRpcUrl, newMintAdd)
+    #userTokenHoldertoMcap = userTokenMcap / userTokenHolders
 
-# TODO: Get user token impression count (lifetime)
-countId = getTweetData.create_twitter_count('historical', userTicker)
-userImpressions = getTweetData.view_count_data(countId)
 
-#Call addAge on each compare token to get the desired conparision date for each
-bonkCompareDate = add_age(bonkLaunchDate, userTokenAge)
-wifCompareDate = add_age(wifLaunchDate, userTokenAge)
-fartcoinCompareDate = add_age(fartcoinLaunchDate, userTokenAge)
-fwogCompareDate = add_age(fwogLaunchDate, userTokenAge)
-gigaCompareDate = add_age(gigaLaunchDate, userTokenAge)
-goatCompareDate = add_age(goatLaunchDate, userTokenAge)
-chillguyCompareDate = add_age(chillguyLaunchDate, userTokenAge)
+    #Call addAge on each compare token to get the desired conparision date for each
+    bonkCompareDate = add_age(bonkLaunchDate, userTokenAge)
+    wifCompareDate = add_age(wifLaunchDate, userTokenAge)
+    fartcoinCompareDate = add_age(fartcoinLaunchDate, userTokenAge)
+    fwogCompareDate = add_age(fwogLaunchDate, userTokenAge)
+    gigaCompareDate = add_age(gigaLaunchDate, userTokenAge)
+    goatCompareDate = add_age(goatLaunchDate, userTokenAge)
+    chillguyCompareDate = add_age(chillguyLaunchDate, userTokenAge)
 
-# Integrate calls to compare functions
+    # Integrate calls to compare functions
+    bonk_mcap_diff, bonk_vol_diff, bonk_mcap, bonk_vol = compare_token_with_benchmark('HistoricalData/bonk-tokenHist.csv', bonkCompareDate, userTokenMcap, userTokenVol)
+    wif_mcap_diff, wif_vol_diff, wif_mcap, wif_vol = compare_token_with_benchmark('HistoricalData/dogwifhatHist.csv', wifCompareDate, userTokenMcap, userTokenVol)
+    fart_mcap_diff, fart_vol_diff, fart_mcap, fart_vol = compare_token_with_benchmark('HistoricalData/FartcoinHist.csv', fartcoinCompareDate, userTokenMcap, userTokenVol)
+    fwog_mcap_diff, fwog_vol_diff, fwog_mcap, fwog_vol = compare_token_with_benchmark('HistoricalData/fwogHist.csv', fwogCompareDate, userTokenMcap, userTokenVol)
+    giga_mcap_diff, giga_vol_diff, giga_mcap, giga_vol = compare_token_with_benchmark('HistoricalData/GigachadHist.csv', gigaCompareDate, userTokenMcap, userTokenVol)
+    goat_mcap_diff, goat_vol_diff, goat_mcap, goat_vol = compare_token_with_benchmark('HistoricalData/GoatseusMaximusHist.csv', goatCompareDate, userTokenMcap, userTokenVol)
+    chillguy_mcap_diff, chillguy_vol_diff, chillguy_mcap, chillguy_vol = compare_token_with_benchmark('HistoricalData/JustAChillGuyHist.csv', chillguyCompareDate, userTokenMcap, userTokenVol)
+
+    #Get twitter impressions
+    userImpressions = get_tweet_impressions(userTicker)
+
+
+    output_array = [[bonk_mcap_diff, bonk_vol_diff, bonk_mcap, bonk_vol],[wif_mcap_diff, wif_vol_diff, wif_mcap, wif_vol],
+                    [fart_mcap_diff, fart_vol_diff, fart_mcap, fart_vol],[fwog_mcap_diff, fwog_vol_diff, fwog_mcap, fwog_vol],
+                    [giga_mcap_diff, giga_vol_diff, giga_mcap, giga_vol], [goat_mcap_diff, goat_vol_diff, goat_mcap, goat_vol],
+                    [chillguy_mcap_diff, chillguy_vol_diff, chillguy_mcap, chillguy_vol], [userImpressions]]
+                    
+                    
+
+
+    #print for testing
+
+    print(f"Bonk Market Cap Difference: {bonk_mcap_diff}%")
+    print(f"Bonk Volume Difference: {bonk_vol_diff}%")
+    print(f"Wif Market Cap Difference: {wif_mcap_diff}%")
+    print(f"Wif Volume Difference: {wif_vol_diff}%")
+    print(f"Fart Market Cap Difference: {fart_mcap_diff}%")
+    print(f"Fart Volume Difference: {fart_vol_diff}%")
+    print(f"Ticker {userTicker} impressions: ")
+    print(userImpressions)
+
+    return output_array
+
+
+
 #Comparision for mcap and vol
 def compare_token_with_benchmark(benchmark_file, compare_date, user_mcap, user_vol):
     benchmark_data = get_benchmark_data(benchmark_file, compare_date, ',')
-    
+    mcap = (benchmark_data['marketCap'].iat[-1])
+    vol = (benchmark_data['volume'].iat[-1])
     mcap_diff = compare_mcap(benchmark_data, user_mcap)
     vol_diff = compare_volume(benchmark_data, user_vol)
     
-    return mcap_diff, vol_diff
+    return mcap_diff, vol_diff, mcap, vol
 
+#Get user token impression count (lifetime)
 def get_tweet_impressions(ticker):
     cashtag = "$" + ticker
 
@@ -92,36 +127,12 @@ def get_tweet_impressions(ticker):
     }
     
     #test count id: "4c30abe4-d62b-427a-a263-e22b020bef3f"
-    count_id = create_twitter_count("7-day", query)
+    count_id = "4c30abe4-d62b-427a-a263-e22b020bef3f"
+    #create_twitter_count("7-day", query)
     time.sleep(10)
     if count_id:
         count_data = view_count_data(count_id)
 
     return count_data
     
-    
-    
-
-# Example usage
-bonk_mcap_diff, bonk_vol_diff = compare_token_with_benchmark('HistoricalData/bonk-tokenHist.csv', bonkCompareDate, userTokenMcap, userTokenVol)
-wif_mcap_diff, wif_vol_diff = compare_token_with_benchmark('HistoricalData/dogwifhatHist.csv', wifCompareDate, userTokenMcap, userTokenVol)
-fart_mcap_diff, fart_vol_diff = compare_token_with_benchmark('HistoricalData/FartcoinHist.csv', fartcoinCompareDate, userTokenMcap, userTokenVol)
-fwog_mcap_diff, fwog_vol_diff = compare_token_with_benchmark('HistoricalData/fwogHist.csv', fwogCompareDate, userTokenMcap, userTokenVol)
-giga_mcap_diff, giga_vol_diff = compare_token_with_benchmark('HistoricalData/GigachadHist.csv', gigaCompareDate, userTokenMcap, userTokenVol)
-goat_mcap_diff, goat_vol_diff = compare_token_with_benchmark('HistoricalData/GoatseusMaximusHist.csv', goatCompareDate, userTokenMcap, userTokenVol)
-chillguy_mcap_diff, chillguy_vol_diff = compare_token_with_benchmark('HistoricalData/JustAChillGuyHist.csv', chillguyCompareDate, userTokenMcap, userTokenVol)
-
-
-
-
-# Repeat for other tokens...
-
-print(f"Bonk Market Cap Difference: {bonk_mcap_diff}%")
-print(f"Bonk Volume Difference: {bonk_vol_diff}%")
-print(f"Wif Market Cap Difference: {wif_mcap_diff}%")
-print(f"Wif Volume Difference: {wif_vol_diff}%")
-print(f"Fart Market Cap Difference: {fart_mcap_diff}%")
-print(f"Fart Volume Difference: {fart_vol_diff}%")
-print(f"Ticker {userTicker} impressions: ")
-print(get_tweet_impressions(userTicker))
-# Print other comparisons...
+main("BktHEAc2WS8TQi2vmavn1rA4L1WJuwF3Vkk3DnwwARti")
