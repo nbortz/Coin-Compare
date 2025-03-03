94306391e2293bdf9a0db1331f069f502515b606
nbortz <nicholas.bortzfield@uvm.edu>
2025-03-03 16:51:49 -0500

    webpage updates

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/", methods=["POST"])
+@app.route("/result", methods=["POST"])
 
 def result():
     if request.method == "POST":
