2aa169e74c88e548f3a51d784d6f7edfc1910041
Michael Scott Hayes <98482126+Mikey1336@users.noreply.github.com>
2025-03-21 12:37:09 -0400

    fixed indexing error into historical data

--- main.py
+++ main.py
@@ -73,13 +73,13 @@ def main(mintAdd):
     chillguyCompareDate = add_age(chillguyLaunchDate, userTokenAge)
 
     # Integrate calls to compare functions
-    bonk_mcap_diff, bonk_vol_diff, bonk_mcap, bonk_vol = compare_token_with_benchmark('HistoricalData/bonk-tokenHist.csv', bonkCompareDate, userTokenMcap, userTokenVol)
-    wif_mcap_diff, wif_vol_diff, wif_mcap, wif_vol = compare_token_with_benchmark('HistoricalData/dogwifhatHist.csv', wifCompareDate, userTokenMcap, userTokenVol)
-    fart_mcap_diff, fart_vol_diff, fart_mcap, fart_vol = compare_token_with_benchmark('HistoricalData/FartcoinHist.csv', fartcoinCompareDate, userTokenMcap, userTokenVol)
-    fwog_mcap_diff, fwog_vol_diff, fwog_mcap, fwog_vol = compare_token_with_benchmark('HistoricalData/fwogHist.csv', fwogCompareDate, userTokenMcap, userTokenVol)
-    giga_mcap_diff, giga_vol_diff, giga_mcap, giga_vol = compare_token_with_benchmark('HistoricalData/GigachadHist.csv', gigaCompareDate, userTokenMcap, userTokenVol)
-    goat_mcap_diff, goat_vol_diff, goat_mcap, goat_vol = compare_token_with_benchmark('HistoricalData/GoatseusMaximusHist.csv', goatCompareDate, userTokenMcap, userTokenVol)
-    chillguy_mcap_diff, chillguy_vol_diff, chillguy_mcap, chillguy_vol = compare_token_with_benchmark('HistoricalData/JustAChillGuyHist.csv', chillguyCompareDate, userTokenMcap, userTokenVol)
+    bonk_mcap_diff, bonk_vol_diff, bonk_mcap, bonk_vol = compare_token_with_benchmark('HistoricalData/bonk-tokenHist.csv', bonkCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    wif_mcap_diff, wif_vol_diff, wif_mcap, wif_vol = compare_token_with_benchmark('HistoricalData/dogwifhatHist.csv', wifCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    fart_mcap_diff, fart_vol_diff, fart_mcap, fart_vol = compare_token_with_benchmark('HistoricalData/FartcoinHist.csv', fartcoinCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    fwog_mcap_diff, fwog_vol_diff, fwog_mcap, fwog_vol = compare_token_with_benchmark('HistoricalData/fwogHist.csv', fwogCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    giga_mcap_diff, giga_vol_diff, giga_mcap, giga_vol = compare_token_with_benchmark('HistoricalData/GigachadHist.csv', gigaCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    goat_mcap_diff, goat_vol_diff, goat_mcap, goat_vol = compare_token_with_benchmark('HistoricalData/GoatseusMaximusHist.csv', goatCompareDate, userTokenMcap, userTokenVol, userTokenAge)
+    chillguy_mcap_diff, chillguy_vol_diff, chillguy_mcap, chillguy_vol = compare_token_with_benchmark('HistoricalData/JustAChillGuyHist.csv', chillguyCompareDate, userTokenMcap, userTokenVol, userTokenAge)
 
     #Get twitter impressions
     userImpressions = get_tweet_impressions(userTicker)
@@ -122,10 +122,11 @@ def main(mintAdd):
 
 
 #Comparision for mcap and vol
-def compare_token_with_benchmark(benchmark_file, compare_date, user_mcap, user_vol):
+def compare_token_with_benchmark(benchmark_file, compare_date, user_mcap, user_vol, userTokenAge):
     benchmark_data = get_benchmark_data(benchmark_file, compare_date, ',')
-    mcap = (benchmark_data['marketCap'].iat[-1])
-    vol = (benchmark_data['volume'].iat[-1])
+    print(benchmark_data)
+    mcap = (benchmark_data['marketCap'].iat[-(userTokenAge)])
+    vol = (benchmark_data['volume'].iat[-(userTokenAge)])
     mcap_diff = compare_mcap(benchmark_data, user_mcap)
     vol_diff = compare_volume(benchmark_data, user_vol)
     
